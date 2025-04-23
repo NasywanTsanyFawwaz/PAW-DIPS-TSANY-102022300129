290de05f5e08d90e5645a7eac353975ceaca4b9c
NasywanTsanyFawwaz <nasywantfawwaz@gmail.com>
2025-04-24 01:42:22 +0700

    commit

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        // ==================2==================
+        // - Set timezone ke Asia/Jakarta
+        // - Buat variabel nama, jam, waktu
+        // - Tentukan $salam berdasarkan jam (pagi, siang, sore, malam)
+        // - Panggil fungsi getTanggal()
+        // - Kirim data ke view 'dashboard' 
+
+        date_default_timezone_set('Asia/Jakarta');
+
+        $nama = "Nasywan Tsany Fawwaz";
+        $jam = date('H');
+        $waktu = date('H:i:s');
+        $tanggal = $this->getTanggal();
+
+        $salam = match (true) {
+            $jam >= 5 && $jam < 12 => 'Selamat Pagi',
+            $jam >= 12 && $jam < 15 => 'Selamat Siang', 
+            $jam >= 15 && $jam < 18 => 'Selamat Sore',
+            default => 'Selamat Malam', 
+
+        }; 
+
+        return view('dashboard', compact('nama', 'jam', 'waktu', 'tanggal', 'salam'));
+    }
+
+    private function getTanggal()
+    {
+        // ==================3==================
+        // - Kembalikan tanggal sekarang dalam format dd-mm-yyyy
+        return date('d-m-Y');
+    }
+}
